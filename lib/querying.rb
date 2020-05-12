6d6863f5002d5f35136b4ff290da35c732f88f92
Neil Empey <argus.two.2@gmail.com>
2020-05-12 22:31:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,6 +35,7 @@ def select_series_title_with_most_human_characters
   ON series.author_id = authors.id
   INNER JOIN characters
   ON authors.id = characters.author_id
+  WHERE characters.species = 'human'
   GROUP BY characters.species ORDER BY characters.species DESC LIMIT 1"
 end
 
